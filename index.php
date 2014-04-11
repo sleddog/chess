8fa4fb249a1497ca6f538559d23d944e48f6bec3
Devin Gray <devingray@gmail.com>
2014-04-10 23:12:49 -0600

    overall of table logic, use buttons instead of spans, initial javascript selection logic

--- index.php
+++ index.php
@@ -1,109 +1,57 @@
 <?php
 /*
 *  Author:  devingray@gmail.com
-*  Very simple chess application written in HTML/Javascript on the frontend that 
+*  Very simple chess application written in PHP/HTML/Javascript on the frontend that 
 *  invokes a CGI program written in C
 */
 
-$white_king = "&#9812;";
-$white_queen  = "&#9813;";
-$white_rook = "&#9814;";
-$white_bishop = "&#9815;";
-$white_knight = "&#9816;";
-$white_pawn = "&#9817;";
-$black_king = "&#9818;";
-$black_queen  = "&#9819;";
-$black_rook = "&#9820;";
-$black_bishop = "&#9821;";
-$black_knight = "&#9822;";
-$black_pawn = "&#9823;";
-
-$starting_positions = array(
-    'white' => array(
-        0 => $white_rook,
-        1 => $white_knight,
-        2 => $white_bishop,
-        3 => $white_queen,
-        4 => $white_king,
-        5 => $white_bishop,
-        6 => $white_knight,
-        7 => $white_rook
-    ),
-    'black' => array(
-        0 => $black_rook,
-        1 => $black_knight,
-        2 => $black_bishop,
-        3 => $black_queen,
-        4 => $black_king,
-        5 => $black_bishop,
-        6 => $black_knight,
-        7 => $black_rook
-    )
+$num_to_letter = array('a','b','c','d','e','f','g','h');
+$pieces = array(
+    'wk'=>"&#9812;",
+    'wq'=>"&#9813;",
+    'wr'=>"&#9814;",
+    'wb'=>"&#9815;",
+    'wn'=>"&#9816;",
+    'wp'=>"&#9817;",
+    'bk'=>"&#9818;",
+    'bq'=>"&#9819;",
+    'br'=>"&#9820;",
+    'bb'=>"&#9821;",
+    'bn'=>"&#9822;",
+    'bp'=>"&#9823;"
 );
+$initial_board_json = '{"a8":"br","b8":"bn","c8":"bb","d8":"bq","e8":"bk","f8":"bb","g8":"bn","h8":"br",';
+$initial_board_json .= '"a7":"bp","b7":"bp","c7":"bp","d7":"bp","e7":"bp","f7":"bp","g7":"bp","h7":"bp",';
+$initial_board_json .= '"a2":"wp","b2":"wp","c2":"wp","d2":"wp","e2":"wp","f2":"wp","g2":"wp","h2":"wp",';
+$initial_board_json .= '"a1":"wr","b1":"wn","c1":"wb","d1":"wq","e1":"wk","f1":"wb","g1":"wn","h1":"wr"}';
 
-function start_pos($index, $color) {
-    global $starting_positions;
-    return $starting_positions[$color][$index];
-}
+function draw_chess_board_from_config($board_json) {
+    global $pieces, $num_to_letter;
 
-function border_width($index) {
-    return '1pt 1pt 1pt 1pt';
-}
+    //json representation of an initial chess board
+    $config = json_decode($board_json);
+    //echo "<pre>".print_r($board_config, true)."</pre>";
 
-function bg_color($index) {
-    if($index % 2 != 0) {
-        return ' bgcolor="silver"';
-    }
-    return '';
-}
-
-function draw_chess_board() {
-    global $white_pawn, $black_pawn;
-
-    $board = '<table style="text-align:center;border-spacing:0pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: 0pt 0pt 0pt 0pt">';
-    $board .= '<tbody>';
-    $board .= '<tr style="vertical-align:bottom;">';
-    $board .= '<td style="vertical-align:middle;width:12pt">8</td>';
-    for($i=0; $i<8; $i++) {
-        $board .= '<td style="width:38pt; height:24pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: ' . border_width($i) . '"' .  bg_color($i) . '><span style="font-size:250%;">' . start_pos($i, 'black') . '</span></td>';
-    }
-    $board .= '</tr>';
-    $board .= '<tr style="vertical-align:bottom;">';
-    $board .= '<td style="vertical-align:middle;width:12pt">7</td>';
-    for($i=0; $i<8; $i++) {
-        $board .= '<td style="width:38pt; height:24pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: ' . border_width($i) . '"' .  bg_color($i+1) . '><span style="font-size:250%;">' . $black_pawn . '</span></td>';
-    }
-    $board .= '</tr>';
-    for($j=0; $j<4; $j++) {
-        $board .= '<tr style="vertical-align:bottom;">';
-        $board .= '<td style="vertical-align:middle;width:12pt">' . (6-$j) . '</td>';
-        for($i=0; $i<8; $i++) {
-            $board .= '<td style="width:38pt; height:24pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: ' . border_width($i) . '"' .  bg_color($i+$j) . '><span style="font-size:250%;">' . '&nbsp;' . '</span></td>';
+    $board = '<table><tbody>';
+    for($i=8; $i>=1; $i--) {
+        $board .= '<tr>';
+        $board .= "<td>$i</td>";
+        for($j=0; $j<8; $j++) {
+            $l = $num_to_letter[$j];
+            $square = "$l$i";
+            $color = (($j+$i) % 2 != 0) ? 'silver' : 'white';
+            $piece = property_exists($config, $square) ? $pieces[$config->$square] : '&nbsp;';
+            $board .= "<td><button id='$square-$color' type='button' style='width:75px; height:75px; font-size: 60px; background-color: $color' onclick='square_select(this)'>$piece</button></td>";
         }
         $board .= '</tr>';
-    }
-    $board .= '<tr style="vertical-align:bottom;">';
-    $board .= '<td style="vertical-align:middle;width:12pt">2</td>';
-    for($i=0; $i<8; $i++) {
-        $board .= '<td style="width:38pt; height:24pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: ' . border_width($i) . '"' .  bg_color($i) . '><span style="font-size:250%;">' . $white_pawn . '</span></td>';
-    }
-    $board .= '</tr>';
-    $board .= '<tr style="vertical-align:bottom;">';
-    $board .= '<td style="vertical-align:middle;width:12pt">1</td>';
-    for($i=0; $i<8; $i++) {
-        $board .= '<td style="width:38pt; height:24pt; border-collapse:collapse; border-color: black; border-style: solid; border-width: ' . border_width($i) . '"' .  bg_color($i+1) . '><span style="font-size:250%;">' . start_pos($i, 'white') . '</span></td>';
     }
     $board .= '</tr>';
     $board .= '<tr>';
     $board .= '<td></td>';
-    $board .= '<td>a</td>';
-    $board .= '<td>b</td>';
-    $board .= '<td>c</td>';
-    $board .= '<td>d</td>';
-    $board .= '<td>e</td>';
-    $board .= '<td>f</td>';
-    $board .= '<td>g</td>';
-    $board .= '<td>h</td>';
+    for($j=0; $j<8; $j++) {
+        $l = $num_to_letter[$j];
+        $board .= "<td align='center'>$l</td>";
+    }
     $board .= '</tr>';
     $board .= '</tbody></table>';
     return $board;
@@ -131,21 +79,44 @@ Devin Gray's chess experiment
  
 <h1>Devin Gray's chess experiment</h1>
 
-<?=draw_chess_board();?>
+<?=draw_chess_board_from_config($initial_board_json);?>
 <div id="next-move"></div>
  
 <script>
 (function() {
   var chessAI = "http://www.devingray.com/cgi-bin/chess_ai.cgi";
   $.getJSON( chessAI, {
-    board: "ASDFG", // idea is to build a hash of the current chess board setup
+    board: '<?=$initial_board_json;?>',
     move: "e2-e4" // this will be a legal chess move
   })
     .done(function( data ) {
-        alert(data);
+        console.log(data);
     });
 })();
+
+var selectedSquare = 0;
+var targetSquare = 0;
+
+function square_select(button) {
+    console.log(button);
+    console.log(button.id);
+    console.log(button.style.backgroundColor);
+    if(selectedSquare == 0) {
+        button.style.backgroundColor="lightgreen";
+        selectedSquare = button.id;
+    }
+    else {
+        if(selectedSquare == button.id) {
+            button.style.backgroundColor=selectedSquare.split('-')[1];
+            selectedSquare = 0;
+        }
+        else { //set target
+            button.style.backgroundColor="lightpink";
+            targetSquare = button.id;
+        }
+    }
+}
 </script>
- 
+
 </body>
 </html>
