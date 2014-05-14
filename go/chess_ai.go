be90c1059b9270ce0dd15809abf609c5732e93ee
Devin Gray <devin.gray@webfilings.com>
2014-05-14 13:21:21 -0600

    add simple go scripts

---
+++ go/chess_ai.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.Seed(time.Now().UTC().UnixNano())
+}
+
+//func main() {
+//	fmt.Println("next move = ", getNextMove())
+//}
+
+func randomColumn() string {
+	var columns string
+	columns = "abcdefgh"
+	return string(columns[rand.Intn(len(columns))])
+}
+
+func getNextMove() string {
+	var move string
+	randChar = randomColumn()
+	move = fmt.Sprintf("\"next-move\":\"%s7-%s5\"", randChar, randChar)
+	return move
+}
---
+++ go/server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/cgi"
+)
+
+func randomMove(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Content-type: text/json\n\n")
+	fmt.Fprintf(w, "{")
+	fmt.Fprintf(w, "\"query\":\"blahhh\",")
+	fmt.Fprintf(w, getNextMove())
+	fmt.Fprintf(w, "}\n")
+}
+
+func main() {
+	http.HandleFunc("/", randomMove)
+	cgi.Serve(nil)
+}
