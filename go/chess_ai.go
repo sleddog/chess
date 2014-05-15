042c1e24e3c96bc99fdd5791201ca85a9c8d2851
Devin Gray <devingray@gmail.com>
2014-05-14 22:20:28 -0600

    add initBoard function and unit test

--- go/chess_ai.go
+++ go/chess_ai.go
@@ -1,18 +1,16 @@
 package chess
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"strconv"
 	"time"
 )
 
 func init() {
 	rand.Seed(time.Now().UTC().UnixNano())
 }
-
-//func main() {
-//	fmt.Println("next move = ", getNextMove())
-//}
 
 func randomColumn() string {
 	var columns string
@@ -26,4 +24,32 @@ func getNextMove() string {
 	randChar = randomColumn()
 	move = fmt.Sprintf("\"next-move\":\"%s7-%s5\"", randChar, randChar)
 	return move
+}
+
+func initBoard(board_json string) [8][8]string {
+	//convert json to string map
+	byt := []byte(board_json)
+	var dat map[string]interface{}
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	//populate board matrix
+	var board [8][8]string
+	for i := 0; i < 8; i++ {
+		for j := 0; j < 8; j++ {
+			letter := numberToLetter(j)
+			square := letter + strconv.Itoa(i+1)
+			if val, ok := dat[square].(string); ok {
+				board[i][j] = val
+			} else {
+				board[i][j] = "0"
+			}
+		}
+	}
+	return board
+}
+
+func numberToLetter(x int) string {
+	letters := []string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	return letters[x]
 }
