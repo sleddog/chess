1136cdd9a0d2479212342bd426655ddc134a751a
Devin Gray <devingray@gmail.com>
2014-05-24 00:44:23 -0600

    fix build script to copy binary into cgi-bin directory

--- build_go.sh
+++ build_go.sh
@@ -1,5 +1,4 @@
 cd ~/public_html/chess/go
-#produces a server binary file in the ./go/bin directory
 go install server
-#TODO create a symlink of ./go/bin/server to ~/public_html/cgi-bin/chess_go_ai.cgi
+cp bin/server ~/public_html/cgi-bin/chess_go_ai.cgi
 cd -
