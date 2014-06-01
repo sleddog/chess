0746f197872d73cb4654d608c1ad75a2d4a839c4
Devin Gray <devingray@gmail.com>
2014-06-01 16:22:54 -0600

    update header comments

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-/*
-*  Author:  Devin Gray <devingray@gmail.com>
-*  Very simple chess application written in PHP/HTML/Javascript on the frontend that 
-*  invokes a CGI program written in Go on the backend
+/* Chess By Devin Gray <devingray@gmail.com>
+*  This is an evolving chess application written by Devin Gray.  The languages used in this application are: PHP, HTML, and Javascript on the front-end, which will invoke a CGI program written in C and/or Go on the back-end.  I will experiment with many types of game modes and AI algorithms.  My goal is to build an AI that can beat me consistently in a head to head match, or at the very least a fun game of chess!
 */
 
 $num_to_letter = array('a','b','c','d','e','f','g','h');
