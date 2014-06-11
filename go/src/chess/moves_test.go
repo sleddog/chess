fb0b47aaab59a69f60a816d889d910df6b5a7059
Devin Gray <devingray@gmail.com>
2014-06-10 22:37:12 -0600

    add a few more tests

--- go/src/chess/moves_test.go
+++ go/src/chess/moves_test.go
@@ -24,3 +24,19 @@ func TestGetLegalMoves(t *testing.T) {
 		t.Error("Initial moves for black should be 20")
 	}
 }
+
+func TestGetKingLocations(t *testing.T) {
+	node := createChessNode(initial_board_json)
+	whiteKingLoc := getKingLocation("w", node.board)
+	fmt.Println("white king location = ", whiteKingLoc)
+	blackKingLoc := getKingLocation("b", node.board)
+	fmt.Println("black king location = ", blackKingLoc)
+}
+
+func TestF7PawnShouldNotMove(t *testing.T) {
+	fenPlacement := "r1bqkbnr/1ppppppp/p7/n6Q/4P3/3P1N2/PPP2PPP/RNB1KB1R"
+	fmt.Println("fen = ", fenPlacement)
+	board := createBoardUsingFen(fenPlacement)
+	node := ChessNode{board: board}
+	node.black_legal_moves = getLegalMoves("b", node.board)
+}
